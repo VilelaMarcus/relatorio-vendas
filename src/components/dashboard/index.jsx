4cf4edd0e4da8e138a5b6e6aea3d07acc0972c48
VilelaMarcus <marcusvilela000@gmail.com>
2023-08-16 10:12:15 -0300

    states working on select

---
+++ src/components/dashboard/index.jsx
@@ -0,0 +1,142 @@
+
+import { Box, MenuItem, Select } from "@mui/material";
+import { MyResponsiveLine } from '../ResponsiveLine';
+import { useEffect, useState } from "react";
+import { products as productData, marcas as brandData } from "../../data/data";
+
+
+const Dashboad = () =>{
+    const [category, setCategory] = useState('Roupa');
+    
+    const [product, setProduct] = useState('');
+    const [productToShow, setProductToShow] = useState([]);
+
+
+    const [brand, setBrand] = useState('');
+    const [brandToShow, setBrandToShow] = useState([]);
+    
+
+    const [lineData, setLineData] = useState()
+
+    useEffect(() => {
+      const payload = productData.find(e => {
+        if(e.type === category) return e.products;
+      })
+
+      setProductToShow(payload?.products);
+      setProduct(payload?.products[0]);
+    }, [category])
+    
+    useEffect(() => {
+      const payload = brandData.filter(brand => {
+        if(brand.type === product) return brand.marca;
+      })
+
+      const brands = payload?.map(e => e.marca)
+      const vendas = payload?.map(e => e.vendas)
+
+      if(vendas[0])
+      {
+        const dataChart = [
+          {
+            id: "VENDAS",
+            data: Object.keys(vendas[0]).map(month => {
+              return {
+                x: month,
+                y: vendas[0][month]
+              };
+            })
+          }
+        ];
+        setLineData(dataChart)
+      }
+      
+      setBrandToShow(brands)
+      setBrand(brands[0])
+    }, [product])
+
+
+    
+  const handleCategoryChange = (event) => {
+    setCategory(event.target.value);
+  }
+
+  const handleProductChange = (event) => {
+    setProduct(event.target.value);
+  }
+
+  const handleBrandChange = (event) => {
+    const value = event.target.value
+
+    const vendas = brandData.filter(e => {
+      if(e.marca === value) return e;
+    })
+    console.log(vendas)
+
+
+    setBrand(value);
+  }
+  
+    return (
+      <Box m="20px">
+        <Box sx={{ display: 'flex',  paddingX: 20, justifyContent: 'space-between'}}>
+          <Box sx={{ minWidth: 120 }}>
+            <Select
+            value={category}
+            label="Categoria"
+            onChange={handleCategoryChange}
+            >
+              <MenuItem value={'Roupa'}>Roupa</MenuItem>
+              <MenuItem value={'Alimento'}>Alimento</MenuItem>
+            </Select>
+          </Box>
+          <Box sx={{ minWidth: 120 }}>
+            <Select
+              value={product}
+              onChange={handleProductChange}
+            >
+              {productToShow.map((e) => {
+                return (
+                  <MenuItem 
+                    key={e}
+                    value={e}
+                  >
+                    {e}
+                  </MenuItem>
+                  )
+              })
+
+              }
+            </Select>
+          </Box>
+          <Box sx={{ minWidth: 120 }}>
+            <Select
+            value={brand}
+            onChange={handleBrandChange}
+            >
+              {brandToShow.map((brand) => {
+                return (
+                  <MenuItem 
+                    key={brand}
+                    value={brand}
+                  >
+                    {brand}
+                  </MenuItem>
+                  )
+              })
+
+              }
+            </Select>
+          </Box>
+        </Box>
+
+        <Box height="75vh">
+          {lineData && ( <MyResponsiveLine data={lineData}/> )}
+        </Box>
+
+
+      </Box>
+    );
+}
+
+export default Dashboad;
--- src/data/data.js
+++ src/data/data.js
@@ -1,2 +1,139 @@
-const category = [{id :1 , type: "Roupa"},  "Alimentos"]
+export const category = ["Roupa",  "Alimentos"];
 
+export const products = [{ type :'Roupa' , products: ['Camisa', 'Calça']} , { type :'Alimento' , products: ['Cereais', ' Fruta']}];
+
+export const marcas = [
+        { 
+            type :'Camisa' ,
+            marca: 'camisa1',
+            vendas :
+                {
+                    janeiro: 100,
+                    fevereiro: 474,
+                    marco: 402,
+                    abril: 536,
+                }
+        }, 
+        { 
+            type :'Camisa' ,
+            marca: 'camisa2',
+            vendas :
+                {
+                    janeiro: 200,
+                    fevereiro: 274,
+                    marco: 402,
+                    abril: 336,
+                }
+        }, 
+        { 
+            type :'Camisa' ,
+            marca: 'camisa3',
+            vendas :
+                {
+                    janeiro: 100,
+                    fevereiro: 374,
+                    marco: 402,
+                    abril: 636,
+                }
+        }, 
+        { 
+            type :'Calça' ,
+            marca: 'Calça1',
+            vendas :
+                {
+                    janeiro: 100,
+                    fevereiro: 474,
+                    marco: 402,
+                    abril: 536,
+                }
+        }, 
+        { 
+            type :'Calça' ,
+            marca: 'Calça2',
+            vendas :
+                {
+                    janeiro: 100,
+                    fevereiro: 474,
+                    marco: 402,
+                    abril: 536,
+                }
+        }, 
+        { 
+            type :'Calça' ,
+            marca: 'Calça3',
+            vendas :
+                {
+                    janeiro: 100,
+                    fevereiro: 474,
+                    marco: 402,
+                    abril: 536,
+                }
+        }, 
+        { 
+            type :'Cereais' ,
+            marca: 'Cereal1',
+            vendas :
+                {
+                    janeiro: 270,
+                    fevereiro: 204,
+                    marco: 392,
+                    abril: 306,
+                }
+        }, 
+        { 
+            type :'Cereais' ,
+            marca: 'Cereal2',
+            vendas :
+                {
+                    janeiro: 100,
+                    fevereiro: 474,
+                    marco: 402,
+                    abril: 536,
+                }
+        }, 
+        { 
+            type :'Cereais' ,
+            marca: 'Cereal3',
+            vendas :
+                {
+                    janeiro: 100,
+                    fevereiro: 474,
+                    marco: 402,
+                    abril: 536,
+                }
+        }, 
+        { 
+            type :'Fruta' ,
+            marca: 'Fruta1',
+            vendas :
+                {
+                    janeiro: 100,
+                    fevereiro: 474,
+                    marco: 402,
+                    abril: 536,
+                }
+        }, 
+        { 
+            type :'Fruta' ,
+            marca: 'Fruta2',
+            vendas :
+                {
+                    janeiro: 100,
+                    fevereiro: 474,
+                    marco: 402,
+                    abril: 536,
+                }
+        }, 
+        { 
+            type :'Fruta' ,
+            marca: 'Fruta3',
+            vendas :
+                {
+                    janeiro: 100,
+                    fevereiro: 474,
+                    marco: 402,
+                    abril: 536,
+                }
+        }, 
+       
+]
